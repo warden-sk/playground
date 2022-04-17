3425abaa0e67b613e179a1f700eda27faca284fa
Marek Kobida <marek.kobida@gmail.com>
2022-04-17 21:20:13 +0200

    moved

--- private/index.tsx
+++ private/index.tsx
@@ -2,16 +2,16 @@
  * Copyright 2022 Marek Kobida
  */
 
+import HorizontalNumberSlider from './HorizontalNumberSlider';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Test from './Test';
 
 function Client() {
   const [price, updatePrice] = React.useState<number>(0);
 
   return (
     <div className="container" mX="auto" p="4">
-      <Test on={updatePrice} size={[25, 100]} step={25} />
+      <HorizontalNumberSlider on={updatePrice} size={[25, 100]} step={25} />
       <div mT="4" textAlign="center">
         {price.toFixed()} EUR
       </div>
