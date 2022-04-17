003352127e04230ac9e2de557b908ab160da1436
Marek Kobida <marek.kobida@gmail.com>
2022-04-17 23:26:50 +0200

    update

--- webpack.ts
+++ webpack.ts
@@ -7,8 +7,9 @@ import webpack from 'webpack';
 
 const compiler = webpack(
   common({
-    htmlTemplate: () => '<div id="client" />',
+    htmlTemplate: () => '<div id="client"></div>',
     name: 'Playground',
+    publicPath: process.env.NODE_ENV === 'production' && 'https://warden-sk.github.io/playground/public',
   })
 );
 
