80871d1793b3131b19943032945f1cfce02c6d48
Marek Kobida <marek.kobida@gmail.com>
2022-06-13 13:24:02 +0200

    update

--- index.tsx
+++ index.tsx
@@ -2,5 +2,6 @@
  * Copyright 2022 Marek Kobida
  */
 
+export { default as Calendar } from './private/Calendar';
 export { default as HorizontalNumberSlider } from './private/HorizontalNumberSlider';
 export { default as HorizontalSlider } from './private/HorizontalSlider';
