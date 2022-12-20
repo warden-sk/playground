a4c865d1d3193d59adf0a9a4297337bfa03f664a
Marek Kobida <marek.kobida@gmail.com>
2022-12-20 15:55:53 +0100

    up

--- private/Calendar/constants.ts
+++ private/Calendar/constants.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 Marek Kobida
+ * Copyright 2023 Marek Kobida
  */
 
 export const DAYS = ['Po', 'Ut', 'St', 'Št', 'Pi', 'So', 'Ne'] as const;
--- private/helpers/readMouseOffset.ts
+++ private/helpers/readMouseOffset.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 Marek Kobida
+ * Copyright 2023 Marek Kobida
  */
 
 function readMouseOffset(event: MouseEvent | TouchEvent): [x: number, y: number] {
